8f76171b0a9650c28267b37218763859184e1614
Lethe1999 <lucaswu1999@gmail.com>
2023-04-13 12:37:53 -0400

    Assignment09

--- server.js
+++ server.js
@@ -29,17 +29,17 @@ app.get('/healthz', (req, res) => {
     metricsClient.timing('duration.endpoint.healthz.get', endTime - startTime);
 });
 
-app.get('/healthzz', (req, res) => {
-    const startTime = new Date();
-    metricsClient.increment("endpoint.health.get");    // Count API calls
-    logger.info("Connect to server succeed");
+// app.get('/health', (req, res) => {
+//     const startTime = new Date();
+//     metricsClient.increment("endpoint.health.get");    // Count API calls
+//     logger.info("Connect to server succeed");
 
-    res.send('Healthy Server');
+//     res.send('Healthy Server');
 
-    // Timer
-    const endTime = new Date();
-    metricsClient.timing('duration.endpoint.health.get', endTime - startTime);
-});
+//     // Timer
+//     const endTime = new Date();
+//     metricsClient.timing('duration.endpoint.health.get', endTime - startTime);
+// });
 
 // Change version
 const publish_version = 'v1';
